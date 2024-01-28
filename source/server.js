0e0c24e37c3d3e01be63a2075ec7472da33d88b2
Stasiv <ptrstasiv@gmail.com>
2024-01-28 13:44:10 +0200

    Data Base test

--- source/server.js
+++ source/server.js
@@ -1,5 +1,22 @@
 const express = require('express')
+const mysql = require("mysql")
 const app = express()
+
+const db = mysql.createConnection({
+  host: "database-meteo.cjaoo4yk2a0q.eu-central-1.rds.amazonaws.com",
+  port: "3306",
+  user: "admin",
+  password: "12345678",
+  database: "meteodb"
+});
+
+db.connect(err => {
+  if(err) {
+    console.log("Error connection to Data Base");
+  }
+
+  console.log("Data Base connected");
+});
 
 app.get('/', (req, res) => {
   res.send('hello world')
